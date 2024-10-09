85f3d27cda7546632c74cd37a78bdba719c93f56
Noobsie09 <rsiegl@tsn.at>
2024-10-09 17:47:41 +0200

    fertig

---
+++ src/Main.java
@@ -0,0 +1,151 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("*****RPG HELL (PART I)*****");
+
+        System.out.println("-------------------------------------------");
+
+        Spieler spieler = new Spieler("Rafael", "Mönch");
+
+        String name = spieler.getName();
+        String klasse = spieler.getKlasse();
+
+        System.out.println("Dein Name ist " + name + " und deine Klasse ist " + klasse + ".");
+
+
+        System.out.println("-------------------------------------------");
+
+
+        System.out.println("Deine Starteigenschaften lauten wie folgt:");
+
+        System.out.println("Level: " + spieler.getLevel());
+        System.out.println("Lebensenergie: " + spieler.getLebensenergie());
+        System.out.println("Zauberenergie: " + spieler.getZauberenergie());
+        System.out.println("Angriff: " + spieler.getAngriff());
+        System.out.println("Präzision: " + spieler.getPraezision());
+        System.out.println("Vitalität: " + spieler.getVitalitaet());
+        System.out.println("Verteidigung: " + spieler.getVerteidigung());
+        System.out.println("Willenskraft: " + spieler.getWillenskraft());
+
+
+        System.out.println("-------------------------------------------");
+
+
+        System.out.println("LEVELANSTIEG!!!");
+
+        //1. Angfriff; 2. Präzision; 3. Vitalität; 4. Verteidigung; 5. Willenskraft
+        spieler.levelAnstieg(false, true, true, false, false);
+        System.out.println("Wähle bis zu 2 Eigenschaften, die du jeweils um 1 erhöhern möchtest, wählst  du nur eine Eigenschaft, wird diese um 2 erhöht");
+
+        System.out.println("Deine Lebens- und Zauberenergie wird zusätzlich um 5% erhöht und dein Level steigt um 1");
+        System.out.println(" ");
+        System.out.println("Deine neuen Eigenschaften sind also: ");
+
+        double Lebensenergie2 = spieler.getLebensenergie();
+        double Zauberenergie2 = spieler.getZauberenergie();
+        int Angriff2 = spieler.getAngriff();
+        int Praezision2 = spieler.getPraezision();
+        int Vitalitaet2 = spieler.getVitalitaet();
+        int Verteidigung2 = spieler.getVerteidigung();
+        int Willenskraft2 = spieler.getWillenskraft();
+
+        System.out.println("Lebensenergie: " + Lebensenergie2);
+        System.out.println("Zauberenergie: " + Zauberenergie2);
+        System.out.println("Angriff: " + Angriff2);
+        System.out.println("Präzision: " + Praezision2);
+        System.out.println("Vitalität: " + Vitalitaet2);
+        System.out.println("Verteidigung: " + Verteidigung2);
+        System.out.println("Willenskraft: " + Willenskraft2);
+
+
+        System.out.println("-------------------------------------------");
+
+
+        System.out.println("LEVELANSTIEG!!!");
+
+        //1. Angfriff; 2. Präzision; 3. Vitalität; 4. Verteidigung; 5. Willenskraft
+        spieler.levelAnstieg(true, true, false, false, false);
+        System.out.println("Wähle bis zu 2 Eigenschaften, die du jeweils um 1 erhöhern möchtest, wählst  du nur eine Eigenschaft, wird diese um 2 erhöht");
+
+        System.out.println("Deine Lebens- und Zauberenergie wird zusätzlich um 5% erhöht und dein Level steigt um 1");
+        System.out.println(" ");
+        System.out.println("Deine neuen Eigenschaften sind also: ");
+
+        double Lebensenergie3 = spieler.getLebensenergie();
+        double Zauberenergie3 = spieler.getZauberenergie();
+        int Angriff3 = spieler.getAngriff();
+        int Praezision3 = spieler.getPraezision();
+        int Vitalitaet3 = spieler.getVitalitaet();
+        int Verteidigung3 = spieler.getVerteidigung();
+        int Willenskraft3 = spieler.getWillenskraft();
+
+        System.out.println("Lebensenergie: " + Lebensenergie3);
+        System.out.println("Zauberenergie: " + Zauberenergie3);
+        System.out.println("Angriff: " + Angriff3);
+        System.out.println("Präzision: " + Praezision3);
+        System.out.println("Vitalität: " + Vitalitaet3);
+        System.out.println("Verteidigung: " + Verteidigung3);
+        System.out.println("Willenskraft: " + Willenskraft3);
+
+
+        System.out.println("-------------------------------------------");
+
+
+        System.out.println("LEVELANSTIEG!!!");
+
+        //1. Angfriff; 2. Präzision; 3. Vitalität; 4. Verteidigung; 5. Willenskraft
+        spieler.levelAnstieg(false, true, false, false, true);
+        System.out.println("Wähle bis zu 2 Eigenschaften, die du jeweils um 1 erhöhern möchtest, wählst  du nur eine Eigenschaft, wird diese um 2 erhöht");
+
+        System.out.println("Deine Lebens- und Zauberenergie wird zusätzlich um 5% erhöht und dein Level steigt um 1");
+        System.out.println(" ");
+        System.out.println("Deine neuen Eigenschaften sind also: ");
+
+        double Lebensenergie4 = spieler.getLebensenergie();
+        double Zauberenergie4 = spieler.getZauberenergie();
+        int Angriff4 = spieler.getAngriff();
+        int Praezision4 = spieler.getPraezision();
+        int Vitalitaet4 = spieler.getVitalitaet();
+        int Verteidigung4 = spieler.getVerteidigung();
+        int Willenskraft4 = spieler.getWillenskraft();
+
+        System.out.println("Lebensenergie: " + Lebensenergie4);
+        System.out.println("Zauberenergie: " + Zauberenergie4);
+        System.out.println("Angriff: " + Angriff4);
+        System.out.println("Präzision: " + Praezision4);
+        System.out.println("Vitalität: " + Vitalitaet4);
+        System.out.println("Verteidigung: " + Verteidigung4);
+        System.out.println("Willenskraft: " + Willenskraft4);
+
+
+        System.out.println("-------------------------------------------");
+
+
+        System.out.println("LEVELANSTIEG!!!");
+
+        //1. Angfriff; 2. Präzision; 3. Vitalität; 4. Verteidigung; 5. Willenskraft
+        spieler.levelAnstieg(false, false, true, false, true);
+        System.out.println("Wähle bis zu 2 Eigenschaften, die du jeweils um 1 erhöhern möchtest, wählst  du nur eine Eigenschaft, wird diese um 2 erhöht");
+
+        System.out.println("Deine Lebens- und Zauberenergie wird zusätzlich um 5% erhöht und dein Level steigt um 1");
+        System.out.println(" ");
+        System.out.println("Deine neuen Eigenschaften sind also: ");
+
+        double Lebensenergie5 = spieler.getLebensenergie();
+        double Zauberenergie5 = spieler.getZauberenergie();
+        int Angriff5 = spieler.getAngriff();
+        int Praezision5 = spieler.getPraezision();
+        int Vitalitaet5 = spieler.getVitalitaet();
+        int Verteidigung5 = spieler.getVerteidigung();
+        int Willenskraft5 = spieler.getWillenskraft();
+
+        System.out.println("Lebensenergie: " + Lebensenergie5);
+        System.out.println("Zauberenergie: " + Zauberenergie5);
+        System.out.println("Angriff: " + Angriff5);
+        System.out.println("Präzision: " + Praezision5);
+        System.out.println("Vitalität: " + Vitalitaet5);
+        System.out.println("Verteidigung: " + Verteidigung5);
+        System.out.println("Willenskraft: " + Willenskraft5);
+    }
+}
---
+++ src/Spieler.java
@@ -0,0 +1,268 @@
+public class Spieler {
+    private String name;
+    private String klasse;
+    private int level;
+    private int angriff;
+    private int praezision;
+    private int vitalitaet;
+    private int verteidigung;
+    private int willenskraft;
+    private double lebensenergie;
+    private double zauberenergie;
+    private double maxLebensenergie;
+    private double maxZauberenergie;
+
+    public Spieler(String name, String klasse)
+    {
+        this.name = name;
+        this.klasse = klasse;
+        this.level = 1;
+        this.maxLebensenergie = 10;
+        this.maxZauberenergie = 10;
+        this.lebensenergie = maxLebensenergie;
+        this.zauberenergie = maxZauberenergie;
+        setStarteigenschaften(klasse);
+    }
+
+    //setter-Methode zum Starteigenschaften je nach Klasse zu setzen
+    private void setStarteigenschaften(String klasse)
+    {
+        if ("Barbar".equals(klasse)) {
+            angriff = 5;
+            praezision = 3;
+            vitalitaet = 2;
+            verteidigung = 2;
+            willenskraft = 5;
+        } else if ("Dämonenjäger".equals(klasse)) {
+            angriff = 4;
+            praezision = 3;
+            vitalitaet = 2;
+            verteidigung = 2;
+            willenskraft = 5;
+        } else if ("Mönch".equals(klasse)) {
+            angriff = 3;
+            praezision = 4;
+            vitalitaet = 3;
+            verteidigung = 5;
+            willenskraft = 5;
+        } else if ("Hexendoktor".equals(klasse)) {
+            angriff = 5;
+            praezision = 3;
+            vitalitaet = 2;
+            verteidigung = 1;
+            willenskraft = 5;
+        } else if ("Zauberer".equals(klasse)) {
+            angriff = 4;
+            praezision = 4;
+            vitalitaet = 2;
+            verteidigung = 2;
+            willenskraft = 5;
+        } else {
+            System.out.println("Ungültige Klasse! Deine jetzige Klasse ist Barbar!");
+            klasse = "Barbar";
+            angriff = 5;
+            praezision = 3;
+            vitalitaet = 2;
+            verteidigung = 2;
+            willenskraft = 5;
+        }
+    }
+
+    //Lebensenergie wird mit angegebenen Wert erhöht aber wird geprüft, dass es nicht über die maximale Lebensenergie hinausschreitet
+    public void erhoehenLebensenergie(double wert)
+    {
+        lebensenergie = Math.min(lebensenergie + wert, maxLebensenergie);
+    }
+
+    //Zauberenergie wird mit angegebenen Wert erhöht aber wird geprüft, dass es nicht über die maximale Zauberenergie hinausschreitet
+    public void erhoehenZauberenergie(double wert)
+    {
+        zauberenergie = Math.min(zauberenergie + wert, maxZauberenergie);
+    }
+
+    //erhöht Angriff, geht aber nicht über 100 Punkte
+    public void erhoehenAngriff()
+    {
+        if (angriff < 100)
+        {
+            angriff = angriff + 1;
+        }
+    }
+
+    //erhöht Präzision, geht aber nicht über 100 Punkte
+    public void erhoehenPraezision()
+    {
+        if (praezision < 100)
+        {
+            praezision = praezision + 1;
+        }
+    }
+
+    //erhöht Vitalität, geht aber nicht über 100 Punkte
+    public void erhoehenVitalitaet()
+    {
+        if (vitalitaet < 100)
+        {
+            vitalitaet = vitalitaet + 1;
+        }
+    }
+
+    //erhöht Verteidigung, geht aber nicht über 100 Punkte
+    public void erhoehenVerteidigung()
+    {
+        if (verteidigung < 100)
+        {
+            verteidigung = verteidigung + 1;
+        }
+    }
+
+    //erhöht Willenskraft, geht aber nicht über 100 Punkte
+    public void erhoehenWillenskraft()
+    {
+        if (willenskraft < 100)
+        {
+            willenskraft = willenskraft + 1;
+        }
+    }
+
+    public void levelAnstieg(boolean angriffWahl, boolean praezisionWahl, boolean vitalitaetWahl, boolean verteidigungWahl, boolean willenskraftWahl)
+    {
+        //Zählen von den gewählten Eigenschaften
+        int anzahlWahlen = 0;
+
+        if (angriffWahl) {
+            anzahlWahlen++;
+        }
+        if (praezisionWahl) {
+            anzahlWahlen++;
+        }
+        if (vitalitaetWahl) {
+            anzahlWahlen++;
+        }
+        if (vitalitaetWahl) {
+            anzahlWahlen++;
+        }
+        if (willenskraftWahl) {
+            anzahlWahlen++;
+        }
+
+        //Level erhöhen, falls Level + 1 höher als 40, dann wird automatisch Level auf 40 gesetzt
+        level = Math.min(level + 1, 40);
+
+        //Überprüfen der Anzahl der gewählten Eigenschaften
+        if (anzahlWahlen < 1 || anzahlWahlen > 2) {
+            System.out.println("Levelanstieg nicht möglich. Bitte wähle genau 1 oder 2 Eigenschaften.");
+        }
+
+
+
+        //Eigenschaften erhöhen
+        //falls anzahlWahlen = 1 dann wird eine Eigenschaft um 2 erhöht, es sei denn, das Level ist über 100
+        else if (anzahlWahlen == 1)
+        {
+            if (angriffWahl)
+            {
+                angriff = Math.min(angriff + 2, 100);
+            }
+            if (praezisionWahl)
+            {
+                praezision = Math.min(praezision + 2, 100);
+            }
+            if (vitalitaetWahl)
+            {
+                vitalitaet = Math.min(vitalitaet + 2, 100);
+            }
+            if (verteidigungWahl)
+            {
+                verteidigung = Math.min(verteidigung + 2, 100);
+            }
+            if (willenskraftWahl)
+            {
+                willenskraft = Math.min(willenskraft + 2, 100);
+            }
+        }
+
+        //falls nicht nur eine gewählt wird, dann werden je nach gewählter Eigenschaft die Punkte verteilt
+        else {
+            if (angriffWahl)
+            {
+                angriff = Math.min(angriff + 1, 100);
+            }
+            if (praezisionWahl)
+            {
+                praezision = Math.min(praezision + 1, 100);
+            }
+            if (vitalitaetWahl)
+            {
+                vitalitaet = Math.min(vitalitaet + 1, 100);
+            }
+            if (verteidigungWahl)
+            {
+                verteidigung = Math.min(verteidigung + 1, 100);
+            }
+            if (willenskraftWahl)
+            {
+                willenskraft = Math.min(willenskraft + 1, 100);
+            }
+        }
+
+        //Energie erhöhen
+        maxLebensenergie *= 1.05;
+        maxZauberenergie *= 1.05;
+        lebensenergie = maxLebensenergie;
+        zauberenergie = maxZauberenergie;
+
+        System.out.println("Neues Level: " + level);
+    }
+
+    //alle Getter-Methoden zu den Attributen
+    public String getName()
+    {
+        return name;
+    }
+    public String getKlasse()
+    {
+        return klasse;
+    }
+    public int getLevel()
+    {
+        return level;
+    }
+    public int getAngriff()
+    {
+        return angriff;
+    }
+    public int getPraezision()
+    {
+        return praezision;
+    }
+    public int getVitalitaet()
+    {
+        return vitalitaet;
+    }
+    public int getVerteidigung()
+    {
+        return verteidigung;
+    }
+    public int getWillenskraft()
+    {
+        return willenskraft;
+    }
+    public double getLebensenergie()
+    {
+        return lebensenergie;
+    }
+    public double getZauberenergie()
+    {
+        return zauberenergie;
+    }
+    public double getMaxLebensenergie()
+    {
+        return maxLebensenergie;
+    }
+    public double getMaxZauberenergie()
+    {
+        return maxZauberenergie;
+    }
+
+}
